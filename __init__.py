9b4dcc18f99518e0cb3570c756aa108852e89dc4
SparkCoder <abhishekdinesh3@gmail.com>
2023-03-12 16:03:49 +0530

    Updated inits

--- __init__.py
+++ __init__.py
@@ -2,17 +2,22 @@ import bpy
 
 from typing import List
 
-from .core.gui import TWEEN_UL_List_Item
+from .core import EventHandler
+
 from .core.gui import TWEEN_UL_List
 from .core.gui import TWEEN_PT_Panel_Main
 
+from .core.operators import TWEEN_OT_Start_Tweens_Op
+from .core.operators import TWEEN_OT_Stop_Tweens_Op
 from .core.operators import TWEEN_OT_Add_Tween_Op
 from .core.operators import TWEEN_OT_Remove_Tween_Op
+from .core.operators import TWEEN_OT_Add_Tween_Target_Op
+from .core.operators import TWEEN_OT_Remove_Tween_Target_Op
 
 from .core.common import Registerable
 
 bl_info = {
-    'name': 'EaseFollow',
+    'name': 'TweenFollow',
     'author': 'SparkCoder',
     'description': '',
     'blender': (2, 80, 0),
@@ -28,8 +33,12 @@ gui_registry: List[Registerable] = [
     TWEEN_PT_Panel_Main,
 ]
 op_registry: List[Registerable] = [
+    TWEEN_OT_Start_Tweens_Op,
+    TWEEN_OT_Stop_Tweens_Op,
     TWEEN_OT_Add_Tween_Op,
     TWEEN_OT_Remove_Tween_Op,
+    TWEEN_OT_Add_Tween_Target_Op,
+    TWEEN_OT_Remove_Tween_Target_Op,
 ]
 
 
@@ -40,12 +49,16 @@ def register():
     # Register guis
     for gui in gui_registry:
         gui.register_cls()
+    # Register events
+    EventHandler.register_cls()
 
 
 def unregister():
-    # Unregister operators
-    for operator in op_registry:
-        operator.unregister_cls()
+    # Unregister events
+    EventHandler.unregister_cls()
     # Unregister guis
     for gui in gui_registry:
         gui.unregister_cls()
+    # Unregister operators
+    for operator in op_registry:
+        operator.unregister_cls()
--- core/operators/__init__.py
+++ core/operators/__init__.py
@@ -1,2 +1,6 @@
+from .op_start_tweens import *
+from .op_stop_tweens import *
 from .op_add_tween import *
 from .op_remove_tween import *
+from .op_add_tween_target import *
+from .op_remove_tween_target import *
